3d23dc3d87bb54ecdb6b8c61221b7035ae729f76
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2020-10-28 10:54:13 +0100

    added module to warehouse_my pom

---
+++ src/main/java/com/foureverhh/warehouse/warehouse_my/WarehouseMyApplication.java
@@ -0,0 +1,13 @@
+package com.foureverhh.warehouse.warehouse_my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseMyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foureverhh/warehouse/warehouse_my/WarehouseMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foureverhh.warehouse.warehouse_my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseMyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
